aa5c1ca11bf5c9c9feb21dcf70cd5d68cc388f70
dplochcoder <daniel.ploch.coder@gmail.com>
2022-11-17 21:55:08 -0500

    Initial server loop

--- SpoilerViewerMod/SpoilerViewerMod/Server/Server.cs
+++ SpoilerViewerMod/SpoilerViewerMod/Server/Server.cs
@@ -1,11 +1,16 @@
-﻿using RandomizerMod.RC;
+﻿using Newtonsoft.Json;
+using RandomizerMod.RC;
 using System;
+using System.ComponentModel;
+using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
 
 namespace SpoilerViewerMod.Server
 {
+    using JsonUtil = PurenailCore.SystemUtil.JsonUtil<SpoilerViewerMod>;
+
     public class Server
     {
         private readonly RandoModContext context;
@@ -15,6 +20,8 @@ namespace SpoilerViewerMod.Server
         {
             this.context = context;
         }
+
+        private byte[] buffer = new byte[1 << 20];
 
         public void RunAsync(int port)
         {
@@ -33,8 +40,67 @@ namespace SpoilerViewerMod.Server
 
             // FIXME: Start JAR
             var handler = socket.Accept();
+            while (true)
+            {
+                var methodName = ReceiveString(handler);
+                switch (methodName)
+                {
+                    case "getMethodContext":
+                        handleMethod<API.RandoContextRequest, API.RandoContext>(handler, getRandoContext);
+                        break;
+                    default:
+                        throw new InvalidOperationException($"Unknown command: {methodName}");
+                }
+            }
+        }
 
-            // FIXME: Protocol
+        private string ReceiveString(Socket socket)
+        {
+            int recv = socket.Receive(buffer, 4, SocketFlags.None);
+            if (recv != 4)
+            {
+                throw new InvalidOperationException($"Expected 4 bytes; got {recv}");
+            }
+
+            int size = BitConverter.ToInt32(buffer, 0);
+            if (size <= 0)
+            {
+                throw new InvalidOperationException($"Expected bytes; got {recv}");
+            }
+
+            recv = socket.Receive(buffer, size, SocketFlags.None);
+            if (recv != size)
+            {
+                throw new InvalidOperationException($"Expected {size} bytes; got {recv}");
+            }
+
+            return System.Text.Encoding.UTF8.GetString(buffer, 0, size);
+        }
+
+        private delegate Resp RpcMethod<Req, Resp>(Req request);
+
+        private void handleMethod<Req, Resp>(Socket socket, RpcMethod<Req, Resp> method)
+        {
+            var req = JsonUtil.DeserializeFromString<Req>(ReceiveString(socket));
+
+            // TODO: Error API
+            var resp = method(req);
+
+            MemoryStream stream = new();
+            using StreamWriter sw = new(stream);
+            JsonUtil.Serialize(resp, sw);
+            sw.Flush();
+
+            Array.Copy(BitConverter.GetBytes(stream.Length), buffer, 4);
+            Array.Copy(stream.GetBuffer(),0, buffer, 4, stream.Length);
+            socket.Send(buffer, 0, (int)(stream.Length + 4), SocketFlags.None);
+        }
+
+        private API.RandoContext getRandoContext(API.RandoContextRequest request)
+        {
+            API.RandoContext rc = new();
+
+            return rc;
         }
     }
 }
